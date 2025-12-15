751e69ddb90991ae178b40fc06874764eb8e776c
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-12-15 10:33:51 +0200

    Config: Init Next.js +TypeScript + TailwindCSS.

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Cairo } from "next/font/google";
+import "./globals.css";
+
+const cairo = Cairo({
+  variable: "--font-cairo",
+  subsets: ["arabic"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "ألمنيوم الجزيرة",
+  description:
+    "مواكبة أحدث التطورات التي تتناسب مع المواصفات الخليجية. ولذلك يعد مصنع ألمنيوم الجزيرة من أهم المصانع التي يمكن الوثوق بها لالتزامها بالدرجة العالية من التصنيع والتركيب والتسليم ومن ثم الخدمة. لذا مازال مصنع ألمنيوم الجزيرة من أكبر الرواد في تلك الصناعة",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body className={`${cairo.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
