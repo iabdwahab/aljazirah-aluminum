f08b1803f294f341b8c64d5a8c68c82ed479f93e
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-22 04:56:44 +0200

    Add optional className prop to SectionTitle.

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,6 +1,14 @@
-export default function SectionTitle({ title }: { title: string }) {
+export default function SectionTitle({
+  title,
+  className,
+}: {
+  title: string;
+  className?: string;
+}) {
   return (
-    <h2 className="bg-linear-to-b from-[#8A5F00] to-[#F0A500] bg-clip-text text-4xl leading-15.25 font-black text-transparent">
+    <h2
+      className={`bg-linear-to-b from-[#8A5F00] to-[#F0A500] bg-clip-text text-4xl leading-15.25 font-black text-transparent ${className}`}
+    >
       {title}
     </h2>
   );
