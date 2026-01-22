8bfd7565634826f795dd0a8b9bea9457f010bf45
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-22 07:21:05 +0200

    Add About page with Qualifications section.

---
+++ components/qualifications/QualificationsSection.tsx
@@ -0,0 +1,9 @@
+import QualificationsSectionHeading from "./QualificationsSectionHeading";
+
+export default function QualificationsSection() {
+  return (
+    <section className="container my-20">
+      <QualificationsSectionHeading />
+    </section>
+  );
+}
---
+++ components/qualifications/QualificationsSectionHeading.tsx
@@ -0,0 +1,19 @@
+import SectionDescription from "../global/SectionDescription";
+import SectionTitle from "../global/SectionTitle";
+
+export default async function QualificationsSectionHeading() {
+  const sectionDataResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_WORDPRESS_ACF_API_URL}/sections-info/78`,
+  );
+  const sectionData = await sectionDataResponse.json();
+
+  return (
+    <div className="grid grid-cols-3 gap-20">
+      <SectionTitle title={sectionData?.acf?.title} />
+      <SectionDescription
+        description={sectionData?.acf?.description}
+        className="col-span-2"
+      />
+    </div>
+  );
+}
