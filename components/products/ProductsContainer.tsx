1b7dfcefe00f15d566025a29bd629dfb4a6ca33d
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-22 02:39:17 +0200

    Improve product card layout responsiveness.

--- components/products/ProductsContainer.tsx
+++ components/products/ProductsContainer.tsx
@@ -8,17 +8,17 @@ export default async function ProductsContainer() {
   const productsList: ProductCardInterface[] = await productsResponse.json();
 
   const productCardClassNames = [
-    "lg:col-start-1 lg:col-span-2 bg-blue-50 flex flex-row-reverse items-center gap-4 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // إطارات
-    "lg:col-start-1 lg:col-span-2 lg:row-start-2 bg-blue-100 flex flex-row items-center gap-4 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // الستائر الزجاجية الجدارية
-    "lg:col-start-3 lg:row-start-1 lg:row-span-2 bg-blue-200 flex flex-col gap-4", // النوافذ القلاب
-    "lg:col-start-1 lg:row-start-1 lg:row-span-2 bg-green-50 flex flex-col gap-10", // الابواب المفصلية
-    "lg:col-start-2 lg:col-span-2 lg:row-start-1 bg-green-100 flex items-center gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // النوافذ المفصلية و ثنائية الأغراض
-    "lg:col-start-2 lg:col-span-2 lg:row-start-2 bg-green-200 flex items-center gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // ألمنيوم ستائر دوارة
-    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-50  flex items-center gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // ألمنيوم ستائر دوارة
-    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-100  flex items-center gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // النوافذ السحاب
-    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-200  flex items-center gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // أبواب المروحة
-    "lg:col-start-3 lg:row-start-1 lg:row-span-3 bg-red-300 flex flex-col gap-10", // القبب السماوية
-    "lg:col-start-3 lg:row-start-4 lg:row-span-3 bg-red-400 flex flex-col gap-10", // الواحات الزجاجية
+    "lg:col-start-1 lg:col-span-2 bg-blue-50 flex flex-col md:flex-row md:items-center gap-4 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // إطارات
+    "lg:col-start-1 lg:col-span-2 lg:row-start-2 bg-blue-100 flex flex-col md:flex-row-reverse md:items-center gap-4 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // الستائر الزجاجية الجدارية
+    "lg:col-start-3 lg:row-start-1 lg:row-span-2 bg-blue-200 flex flex-col md:flex-row lg:flex-col gap-4", // النوافذ القلاب
+    "lg:col-start-1 lg:row-start-1 lg:row-span-2 bg-green-50 flex flex-col md:flex-row-reverse lg:flex-col gap-10", // الابواب المفصلية
+    "lg:col-start-2 lg:col-span-2 lg:row-start-1 bg-green-100 flex md:items-center flex-col md:flex-row gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // النوافذ المفصلية و ثنائية الأغراض
+    "lg:col-start-2 lg:col-span-2 lg:row-start-2 bg-green-200 flex md:items-center flex-col md:flex-row-reverse gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // ألمنيوم ستائر دوارة
+    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-50 flex md:items-center flex-col md:flex-row gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // ألمنيوم ستائر دوارة
+    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-100 flex md:items-center flex-col md:flex-row-reverse gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // النوافذ السحاب
+    "lg:col-start-1 lg:col-span-2 lg:row-span-2 bg-red-200 flex md:items-center flex-col md:flex-row gap-10 [&>*:first-child]:basis-1/3 [&>*:last-child]:basis-2/3", // أبواب المروحة
+    "lg:col-start-3 lg:row-start-1 lg:row-span-3 bg-red-300 flex flex-col md:flex-row-reverse lg:flex-col gap-10", // القبب السماوية
+    "lg:col-start-3 lg:row-start-4 lg:row-span-3 bg-red-400 flex flex-col md:flex-row lg:flex-col  gap-10", // الواحات الزجاجية
   ];
 
   const finalProductsList = productsList.map(
