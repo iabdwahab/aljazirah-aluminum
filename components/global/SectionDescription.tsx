816ddd527b9aeb472094bb2ded8b347012a71ec9
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-22 06:48:16 +0200

    Add className prop to SectionDescription component.

--- components/global/SectionDescription.tsx
+++ components/global/SectionDescription.tsx
@@ -1,7 +1,11 @@
 export default function SectionDescription({
   description,
+  className = "",
 }: {
   description: string;
+  className?: string;
 }) {
-  return <p className="leading-8.25 font-medium">{description}</p>;
+  return (
+    <p className={`leading-8.25 font-medium ${className}`}>{description}</p>
+  );
 }
