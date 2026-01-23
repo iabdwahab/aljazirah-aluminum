3ee413b383556f4aafb2e7ae4a367ef99b028b64
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-23 16:58:11 +0200

    Redesigning OurWorkCard.

--- components/our-work/OurWorkCard.tsx
+++ components/our-work/OurWorkCard.tsx
@@ -1,41 +1,54 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function OurWorkCard() {
   return (
-    <article className="relative z-10 grid h-125 max-h-125 grid-cols-2 overflow-hidden rounded-4xl rounded-bl-none bg-gray-900 p-4">
-      <div className="relative">
-        <Image
-          src="/work/work-1.png"
-          alt="Our Work 1"
-          className="absolute right-10 -z-10 w-full rotate-15"
-          width={415}
-          height={472}
-        />
+    <article className="relative flex h-[472px] flex-col justify-between space-y-10 overflow-hidden rounded-4xl bg-black text-white">
+      <span className="absolute top-0 left-1/4 h-full w-full rounded-full bg-[#ffffff38] blur-[200px]"></span>
+
+      <div className="grid px-4 pt-6 md:grid-cols-2">
+        <div className="relative hidden md:block">
+          <Image
+            src="/work/work-1.png"
+            alt="our work 1"
+            width={550}
+            height={1120}
+            className="absolute right-15 w-[90%] rotate-15"
+          />
+        </div>
+        <div className="relative">
+          <h3>القبب السماوية (السكاي لايت)</h3>
+          <p>
+            ⁠تصميم هندسي يسمح بدخول الإضاءة الطبيعية بأفضل توزيع داخل المساحات
+            الداخلية. •⁠ ⁠هيكل ألمنيوم عالي الجودة مقاوم للعوامل الجوية والظروف
+            المناخية القاسية. •⁠ ⁠زجاج مختار بعناية يوفر عزلًا حراريًا وصوتيًا
+            ممتازًا. •⁠ ⁠نظام تركيب محكم يمنع تسرب المياه والغبار. •⁠ ⁠مناسب
+            للأسطح، الممرات، الصالات، والمباني السكنية والتجارية
+          </p>
+        </div>
       </div>
 
-      <div className="pt-8 text-[rgba(241,241,241,1)]">
-        <h3 className="text-2xl leading-15.25 font-bold">
-          بــــــــاب زجاجـــــــي
-        </h3>
-        <p className="">
-          باب زجاجي أنيق بإطار من اﻷلمنيوم المعالج ضد الصدأ والرطوبة، يتميز
-          بتصميمه العصري وإغﻼقه المحكم الذي يمنع دخول الغبار، يجمع بين البساطة
-          والجودة، ويمنح المكان مظهرا واسعا ومضيئاً
-        </p>
+      <div className="relative mr-auto w-fit rounded-tr-4xl bg-black p-4">
+        <Link
+          href="#"
+          className="relative flex h-[80px] w-fit items-center justify-center rounded-[20px] bg-[#FFFFFF1A] px-6"
+        >
+          <Image
+            src="/unlabeled/arrow.svg"
+            alt="arrow icon"
+            className="w-full"
+            width={84}
+            height={15}
+          />
+        </Link>
+
+        <div className="absolute bottom-full left-0 h-8 w-8 overflow-hidden bg-black">
+          <span className="block h-[200%] w-[200%] translate-x-1/2 -translate-y-1/2 rounded-full bg-[#080808]"></span>
+        </div>
+        <div className="absolute bottom-0 left-full h-8 w-8 overflow-hidden bg-black">
+          <span className="block h-[200%] w-[200%] translate-x-1/2 -translate-y-1/2 rounded-full bg-[#0E0E0E]"></span>
+        </div>
       </div>
-
-      <button className="absolute bottom-0 left-0 z-10 h-20 w-35 rounded-2xl bg-amber-200 text-black">
-        ---------
-      </button>
-
-      <span className="absolute bottom-0 left-0 -z-10 h-25 w-40 rounded-tr-4xl bg-black"></span>
-
-      <span className="absolute bottom-25 left-0 h-10 w-10 overflow-hidden bg-black">
-        <span className="block h-[200%] w-[200%] translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900"></span>
-      </span>
-      <span className="absolute bottom-0 left-40 h-10 w-10 overflow-hidden bg-black">
-        <span className="block h-[200%] w-[200%] translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900"></span>
-      </span>
     </article>
   );
 }
