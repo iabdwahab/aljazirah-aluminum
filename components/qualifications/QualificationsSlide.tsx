90bafec119fd238f314bf45acb38be92a51dcee2
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-01-24 08:27:18 +0200

    Refactor QualificationsSlide to support one_column layout.

--- components/qualifications/QualificationsSlide.tsx
+++ components/qualifications/QualificationsSlide.tsx
@@ -7,7 +7,7 @@ export default function QualificationsSlide({
   slideData: QualificationInterface;
 }) {
   const {
-    acf: { slide_type, image },
+    acf: { slide_type, image, title, description, grid_data },
   } = slideData;
 
   if (slide_type === "grid") {
@@ -38,7 +38,7 @@ export default function QualificationsSlide({
 
   return (
     <article
-      className={`relative z-10 grid h-full grid-cols-2 grid-rows-3 gap-10 overflow-hidden rounded-3xl bg-cover bg-center px-28 py-16 text-white`}
+      className={`${slide_type === "one_column" ? "space-y-10" : "grid grid-cols-2"} relative z-10 h-full overflow-hidden rounded-3xl bg-cover bg-center px-28 py-16 text-white`}
     >
       <Image
         src={image || "/qualifications/work-1.jpg"}
@@ -47,16 +47,15 @@ export default function QualificationsSlide({
         width={4096}
         height={2731}
       />
-
       <span className="absolute top-0 left-0 -z-10 h-full w-full bg-linear-to-b from-[#00000000] to-[#292929]"></span>
 
-      {Object.values(slideData.acf.grid_data || {}).map((item, index) => (
+      <h3 className="text-4xl font-black">{title}</h3>
+      {description && (
         <div
-          key={index}
-          className="text-2xl text-white"
-          dangerouslySetInnerHTML={{ __html: item }}
+          dangerouslySetInnerHTML={{ __html: description }}
+          className={`${slide_type === "one_column" ? "" : "space-y-2"} text-2xl font-bold`}
         ></div>
-      ))}
+      )}
     </article>
   );
 }
